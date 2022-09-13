c6e6b1d149cff9e61b3eb391a679354be2147c70
Luan Reinhold <anjobranco67@gmail.com>
2022-09-13 18:17:57 -0300

    add componente header adm

--- site/src/App.js
+++ site/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
+import '../src/common/common.scss'
+import HeaderAdmin from './components/adminHeader';
 
 function App() {
   return (
  <section>
   <h1> Subindo projeto pro git</h1>
-
-
+   <HeaderAdmin></HeaderAdmin>
  </section>
-  );
+  )
 }
 
 export default App;
---
+++ site/src/components/adminHeader/index.js
@@ -0,0 +1,12 @@
+import './index.scss'
+
+export default function HeaderAdmin () {
+
+    return (
+        <section className="headerAdmin">
+            <img src="./logoStoreGames.png" alt="img logo" />
+            <div className="boxText"> <p>Seja bem vindo, <span id='adm'>Administrador</span></p> </div>
+            
+        </section>
+    )
+}
