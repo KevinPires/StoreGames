a3314d699e4485ce624ce066123ed3ef3b9e3304
gabrielferreira <gf2627651@gmail.com>
2022-09-16 17:42:37 -0300

    Merge branch 'master' of https://github.com/KevinPires/StoreGames

--- site/src/pages/admin/cadastrarJogos/index.js
+++ site/src/pages/admin/cadastrarJogos/index.js
@@ -1,29 +1,22 @@
 import './index.scss'
 
+import HeaderAdmin  from '../../../components/adminHeader'
+import BarraLateral from '../../../components/BarraLateral'
+
 
 
 export default function CadastratJogos(){
     return (
   
-       <main className="cadastrar-jogos-page">
+      <main className="cadastrar-jogos-page">
           
-          <section className="barra-lateral">
-            <div className="atalho"> <img className="casa" src="/casa.png" alt="casa" />        <p>Home</p></div>
-            <div className="atalho"> <img className="casa" src="/maispreto.png" alt="casa" />   <p> Cadastrar o Jogos</p></div>
-            <div className="atalho"> <img className="casa" src="/lupa.png" alt="casa" />        <p>Consultar pedidos</p></div>
-            <div className="atalho"> <img className="casa" src="/pendentes.png" alt="casa" />   <p>Pedidos Pendentes</p></div>
-            <div className="atalho"> <img className="casa" src="/concluidos.png" alt="casa" />  <p>Pedidos Concluidos</p></div>
-            <div className="atalho1"> <img className="casa" src="/sair.png" alt="casa" />       <p>Sair</p></div>
-            
-          </section>
+          <BarraLateral/>
+          <div className="cont-faixa-cadastro">
+            <HeaderAdmin/>
+          </div>
+          
        
-        </main>
+      </main>
 
-
-
-
-
-
-       
     )
 }
