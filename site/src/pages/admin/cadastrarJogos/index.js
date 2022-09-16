161c177b57c3ffc5e74f6917c99199d75f826c79
Luan Reinhold <anjobranco67@gmail.com>
2022-09-16 08:46:14 -0300

    esqueleto pastas

--- site/src/pages/admin/cadastrarJogos/index.js
+++ site/src/pages/admin/cadastrarJogos/index.js
@@ -1,12 +1,12 @@
 import './index.scss'
-import HeaderAdmin from '../../components/adminHeader'
-import BarraLateral from '../../components/BarraLateral'
+
 
 
 export default function CadastratJogos(){
     return (
   
         <section className="page">
+            
           
         </section>
 
