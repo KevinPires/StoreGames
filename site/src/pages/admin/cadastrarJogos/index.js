e03fa8998c29d3653004b7c10e03ed585f8223ce
Luan Reinhold <anjobranco67@gmail.com>
2022-09-18 15:51:25 -0300

    ajuste

--- site/src/pages/admin/cadastrarJogos/index.js
+++ site/src/pages/admin/cadastrarJogos/index.js
@@ -3,6 +3,7 @@ import './index.scss'
 import HeaderAdmin  from '../../../components/adminHeader'
 import BarraLateral from '../../../components/BarraLateral'
 import TelaCadastro from '../../../components/telaCadastro'
+import InputCadastro from '../../../components/inputCadastro'
 
 
 export default function CadastratJogos(){
@@ -18,7 +19,73 @@ export default function CadastratJogos(){
 
             <section className="container-Column">
               
-              <TelaCadastro/>
+            <section className="pageCadastro">
+              
+              <section className="containerCadastrar">
+                  <section className="header">
+                      <p>Cadastrar Jogos</p>
+                  </section>
+
+                  <section className="container1">
+
+                      <div className="labelInput">
+                          <label>Nome:</label>
+                          <InputCadastro/>
+                      </div>
+
+                      <div className="labelInput">
+                          <label htmlFor="">Genero:</label> 
+                          <select name="generos" id="generos"><option></option></select>
+                      </div>
+
+                      <div className="labelInput">
+                          <label htmlFor="">Plataforma:</label> 
+                          <select name="generos" id="generos"><option></option></select>
+                      </div>
+
+                  </section>
+
+                  <section className="container2">
+                      
+                      <div className="boxColumn">
+                          <label htmlFor="">Descrição</label>             
+                          <textarea name="" id="" cols="43" rows="8"></textarea> 
+                      </div>
+
+                      <div className="boxColumn">
+                          <label htmlFor="">Requisitos Minimos</label>    
+                          <textarea name="" id="" cols="43" rows="8"></textarea>
+                      </div>
+
+                      <div className="boxColumn">
+                          <label htmlFor="">Imagem</label>                
+                          <textarea name="" id="" cols="20" rows="10"></textarea>
+                      </div>
+
+                  </section>
+
+                  <section className="container3">
+
+                      <div className="labelInput">
+                          <label>Disponivel:</label>                         
+                          <select name="generos" id="generos"><option></option></select>
+                      </div>
+                      <div className="labelInput">
+                          <label htmlFor="">Valor</label>                    
+                          <InputCadastro/>
+                      
+                      </div>
+                      <div className="labelInput">
+                          <label htmlFor="">Quantidade</label>               
+                          <InputCadastro/>
+                      </div>
+
+                  </section>
+                  <section className="faixa-botao"><button>Cadastrar Jogo</button></section>
+                  
+              </section>
+        </section>
+    
             
             </section>
             
