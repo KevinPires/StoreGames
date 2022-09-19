7f24c60e1da0dcafc25d4e2e5525c5b8b2fbb951
KevinPires <89464152+KevinPires@users.noreply.github.com>
2022-09-19 16:52:39 -0300

    Small fix login user

--- DB/dml.sql
+++ DB/dml.sql
@@ -2,5 +2,5 @@ use storegames;
 
 INSERT INTO TB_ADMIN (DS_NOME)
 	values ('Luan');
-    INSERT INTO TB_ADMIN_LOGIN (FK_ADMIN, DS_EMAIL, DS_SENHA)
-	values (1,'Luan@gmail.com', '1234');
+    INSERT INTO TB_ADMIN_LOGIN (FK_ADMIN, DS_LOGIN, DS_SENHA)
+	values (1,'Luan', '1234');
