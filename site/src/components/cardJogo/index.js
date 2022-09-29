662ce4c15f6e6f05f4854063d3f67c586f669ce3
Luan Reinhold <anjobranco67@gmail.com>
2022-09-29 17:19:06 -0300

    add componente card, add layout pagina da loja

---
+++ site/src/components/cardJogo/index.js
@@ -0,0 +1,12 @@
+import './index.scss'
+
+
+export default function CardJogo (props) {
+    return (
+        <section className="BoxCard" >
+                <img src="./capagta.jpg" alt="" />
+                <h4>Grand theft auto V, Multiplataforma - key/midia física</h4>
+                <span id='card-preco'>R$60.00</span>
+        </section>
+    )
+}
--- site/src/pages/loja/areaDaLoja/index.js
+++ site/src/pages/loja/areaDaLoja/index.js
@@ -1,6 +1,8 @@
 import BarraVantagens  from '../../../components/TelaDeJogos/vantagens'
 import HeaderLoja from '../../../components/headerLoja'
 import './index.scss'
+import '../../../common/common.scss'
+import CardJogo from '../../../components/cardJogo'
 
 export default function lojaArea () {
 
@@ -11,7 +13,29 @@ export default function lojaArea () {
             <section className="faixaum">  
                 <p>Compre o seu jogo aqui</p>
             </section>
+
            <BarraVantagens/>
+            <section className='container flexboxrow'>
+                <div className="container-filtros">
+
+                </div>
+
+                <div className='container-jogos'>
+                <CardJogo/>
+                <CardJogo/>
+                <CardJogo/>
+                <CardJogo/>
+                <CardJogo/>
+                <CardJogo/>
+                <CardJogo/>
+                <CardJogo/>
+                <CardJogo/>
+                <CardJogo/>
+                <CardJogo/>
+                <CardJogo/>
+                </div>
+            </section>
+
         </main>
     )
 }
