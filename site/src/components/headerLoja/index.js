48122819916f3d91bd564c5ef23148d85fc61ec8
Luan Reinhold <anjobranco67@gmail.com>
2022-10-06 23:52:59 -0300

    add filtrar jogo nome loja

--- site/src/components/headerLoja/index.js
+++ site/src/components/headerLoja/index.js
@@ -1,16 +1,33 @@
+import { useEffect, useState } from 'react'
 import '../../../src/common/common.scss'
+import { listarTodosJogos, listarTodosJogosPorNome } from '../../api/jogos'
 
 import InputPesquisa from '../inputPesquisa'
+
 import './index.scss'
 export default function HeaderLoja () {
 
+    const [texto, setTexto] = useState('')
+
+   async function filtrar() {
+        const resp = await listarTodosJogosPorNome(texto)
+        // passar a resp como parametro no array de filmes para filtrar
+   }
+
+   useEffect(() => {
+        filtrar()
+   }, [texto])
+  
     return (
         <main className="Header">
 
                 <section className="containerPesquisa">
                     <img id="formatLogo" src="./logo.png" alt="logo" />
-                    <InputPesquisa nome="Pesquise por jogos"/>
 
+                    <section className="input-Pesquisa" >   
+                        <input type="text" placeholder='Pesquisar Jogo' className='input-pesquisa-format' value={texto} onChange={e => setTexto(e.target.value)} />
+                    </section>
+                    
                     <div className="boxUsuario">
                         <img id='svgIcon' src="./Icon.svg" alt="iconUser" />
                         <span>Bem vindo, Fulano <a>Minha Conta</a> | <a>Sair</a></span>
