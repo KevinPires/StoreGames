550521337908cf7444f210fa380f24d31e8c2df5
gabrielferreira <gf2627651@gmail.com>
2022-10-17 16:52:07 -0300

    pagina produto

--- site/src/pages/loja/detalhesProduto/index.js
+++ site/src/pages/loja/detalhesProduto/index.js
@@ -1,6 +1,6 @@
 import './index.scss'
 import HeaderLoja from '../../../components/headerLoja'
-
+import Rodape from '../../../components/Rodapé'
 
 
 export default function DetalhesProduto(){
@@ -10,7 +10,7 @@ export default function DetalhesProduto(){
             <div className='box-cima'>
                 <div className='bloc-img'>
                     <img src='/Imagem GTA.png' alt=''/>
-                    <div className='display-row'>
+                    <div className='favorito'>
                         <img src='/coracaoIcon.png' alt='' />
                         <p>Adiciona aos <br/>favoritos</p>
                     </div>
@@ -45,10 +45,48 @@ export default function DetalhesProduto(){
                             </div>
                             <img src='/logoStoreGames.png' alt=''/>    
                         </div>
-                        
+                        <div>
+                            <img src='bandeirasCartao.png' alt=''/>
+                        </div>
+                       
+                       <button className='bt-adic'>Adicionar ao carrinho</button>
                     </div>
                 </div>
             </div>
+
+            <div className='box-baixo'>
+                <div className='bloc-req'>
+                    <h2>Requisitos minimos</h2>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
+                    </p>
+
+                </div>
+                <div className='bloc-jogos'>
+                    <h2>Jogos do mesmo gênero</h2>
+                    <div className='jogos'>
+                        <div>
+                            <img  className='img-gta' src='/Imagem GTA.png' alt=''/>
+                            <h4>Grand Theft Auto V </h4>
+                        </div>
+                        <div>
+                            <img  className='img-gta' src=' /Imagem GTA.png' alt=''/>
+                            <h4>Grand Theft Auto V </h4>
+                        </div>
+                        <div>
+                            <img className='img-gta' src='/Imagem GTA.png' alt=''/>
+                            <h4>Grand Theft Auto V </h4>
+                        </div>
+                        <div>
+                            <img className='img-gta' src='/Imagem GTA.png' alt=''/>
+                            <h4>Grand Theft Auto V </h4>
+                        </div>
+                    </div>
+                    <button className='btn-loja'>Ir para loja</button>
+                </div>
+            </div>
+            <Rodape/>
         </main>
     )
 }
