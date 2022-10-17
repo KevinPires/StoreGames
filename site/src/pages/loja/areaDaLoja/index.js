6be22ee629feaa6466ce58e99f2f87c912bc4ce5
luan r <anjobranco67@gmail.com>
2022-10-17 11:42:48 -0300

    ajuste header

--- site/src/pages/loja/areaDaLoja/index.js
+++ site/src/pages/loja/areaDaLoja/index.js
@@ -48,7 +48,28 @@ export default function LojaArea () {
     const paginate = pageNumber => setCurrentPage(pageNumber)
     return (
         <main className="lojaPage">
-            <HeaderLoja/>
+                    <section className="HeaderLoja">
+                        <section className="containerPesquisa">
+                            <img id="formatLogo" src="/logo.png" alt="logo" />
+
+                            <section className="input-Pesquisa" >   
+                                <input type="text" placeholder='Pesquisar Jogo' className='input-pesquisa-format' value={texto} onChange={e => setTexto(e.target.value)} />
+                            </section>
+
+                            <div className="boxUsuario">
+                                <img id='svgIcon' src="/Icon.svg" alt="iconUser" />
+                                <span>Bem vindo, Fulano <a>Minha Conta</a> | <a>Sair</a></span>
+                            </div>
+                        </section>
+                        <section className="containerCategoria">
+                            <li className='listaOpcoes'>
+                                <Link to='/jogos'>Loja</Link>
+                                <p>Categorias</p>
+                            <Link to='/jogos'>Suporte</Link>
+                                <p>Área do Usuario</p>
+                            </li>
+                        </section>
+                    </section>
             <section className='container-espacamanto'>
 
             </section>
