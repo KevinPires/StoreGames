e67c0ce8f273204a9ae946756808a71cbabef82b
Luan Reinhold <anjobranco67@gmail.com>
2022-10-17 17:17:11 -0300

    renderizacao condicional carrinho vazio

--- site/src/pages/carrinhoCompras/carrinho/index.js
+++ site/src/pages/carrinhoCompras/carrinho/index.js
@@ -1,16 +1,31 @@
 
+import { useState } from 'react'
 import '../../../common/common.scss'
 import HeaderLoja from '../../../components/headerLoja'
 import EtapasImagens from '../../../components/imagensCarrinho'
 import Rodape from '../../../components/Rodapé'
 import './index.scss'
 
-export default function carrinhoCompras() {
+export default function CarrinhoCompras() {
+
+    const [mostrarJogo, setMostrarJogo] = useState(true)
 
     return (
+
         <main className="carrinho-page">
             <HeaderLoja/>
+            {mostrarJogo === false &&
+            <section className="carrinho-vazio">
+                <div className='info-carrinho-vazio'>
+                    <h1>Seu carrinho está vazio!</h1>
+                    <h1>Deseja procurar por produtos similares?</h1>
+                </div>
 
+                <button>Continuar Comprando</button>
+            </section>
+            }
+
+            {mostrarJogo === true && 
             <section className="faixa-carrinho">
                 <div className="faixa-imagens">
                     <EtapasImagens/>
@@ -33,24 +48,19 @@ export default function carrinhoCompras() {
                             </div>
                         </div>
 
-                        
-
-
                     </div>
                  
-
                     <div className="box-info">
                         <div className="flexboxcolumn">
                             <div className="flexboxrow"><span>Preço Total: </span> <span>R$ Dinheiros</span></div>
                             <div className="flexboxrow"><span>Frete: </span> <span>R$ Dinheiros</span></div>
                         </div>
-
                         <button>Continuar Compra</button>
                     </div>
                 </div>
-
-
+                
             </section>
+            }
 
             <Rodape/>
         </main>
