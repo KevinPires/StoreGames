05d81db5fa3df3b9c6c00fba2d5016128413686f
KevinPires <89464152+KevinPires@users.noreply.github.com>
2022-10-30 20:32:38 -0300

    Fix and api finished

--- site/src/components/TelaDeJogos/barraFiltros/index.js
+++ site/src/components/TelaDeJogos/barraFiltros/index.js
@@ -1,18 +1,25 @@
-import './index.scss';
 import './index.scss'
+import { filtrarValorCem } from '../../../api/jogos'
+import React, { useState } from "react"
 
 
 export default function BarraFilto(){
+
+    const [filtro50, setFiltro50 ] = useState()
+
+    async function filtroFifty(){
+        const resposta = await filtrarValorCem()
+    }
     return(
         <div className="comp-filtro">
             <div className="container">
                 <div className="box-filtro">
                     <h4>Filtrar por preço</h4>
                     <div className="" >
-                        <div className="camp-filtro"> <input type="checkbox" className="checkbox-round"/> <p>Menos de R$50,00</p></div>
-                        <div className="camp-filtro"> <input type="checkbox" className="checkbox-round"/> <p>de R$ 51,00 até R$100,00 </p> </div>
-                        <div className="camp-filtro"> <input type="checkbox" className="checkbox-round"/> <p>de R$ 101,00 até R$200,00 </p> </div>
-                        <div className="camp-filtro"> <input type="checkbox" className="checkbox-round"/> <p>de R$ 201,00 </p> </div>
+                        <div className="camp-filtro"> <input type="radio" name="valor" className="checkbox-round" onClick={setFiltro50}/> <p>Menos de R$50,00</p></div>
+                        <div className="camp-filtro"> <input type="radio" name="valor" className="checkbox-round"/> <p>de R$ 51,00 até R$100,00 </p> </div>
+                        <div className="camp-filtro"> <input type="radio" name="valor" className="checkbox-round"/> <p>de R$ 101,00 até R$200,00 </p> </div>
+                        <div className="camp-filtro"> <input type="radio" name="valor" className="checkbox-round"/> <p>de R$ 201,00 </p> </div>
                     </div>
                 </div>
                 <div className="box-filtro">                
