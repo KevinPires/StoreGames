0fe8313eb1f5ebe4d73f47722c08cd695693c4f8
Vidal <guilhermevidalpekka@gmail.com>
2022-10-31 18:44:20 -0300

    tabelas prontas

--- site/src/pages/admin/pedidosConcluidos/index.js
+++ site/src/pages/admin/pedidosConcluidos/index.js
@@ -11,6 +11,42 @@ export default function pedidosConcluidos(){
     <div className="cont-faixa-cadastro">
 
          <HeaderAdmin/>
+         <div className='conteudo'>
+            <div className='caixa-busca'>
+                <input type="text" placeholder='Buscar pedidos concluidos  por nome' />
+            </div>
+        
+
+            <table >
+                <thead>
+                    <tr>
+                        <th>NOME DO CLIENTE</th>
+                        <th>DATA</th>
+                        <th>PREÇO</th>
+                        <th>MÉTODO DE PAGAMENTO</th>
+                        <th>PRODUTO</th>
+                      
+                        
+                    </tr>
+                </thead>
+                <tbody>
+
+                
+                <tr>
+                        <th>Joaquim</th>
+                        <th>22/10/2022</th>
+                        <th>R$ 30,00</th>
+                        <th>Cartão</th>
+                        <th>Terraria</th>
+                        
+                        
+            
+                    </tr>
+                    
+                
+                </tbody>
+            </table>
+        </div>
      </div>
        </main>
     )
--- site/src/pages/admin/pendentes/index.js
+++ site/src/pages/admin/pendentes/index.js
@@ -11,6 +11,52 @@ export default function Pendentes(){
     <div className="cont-faixa-cadastro">
 
          <HeaderAdmin/>
+
+         <div className='conteudo'>
+            <div className='caixa-busca'>
+                <input type="text" placeholder='Buscar pedidos pendentes por nome' />
+            </div>
+        
+
+            <table >
+                <thead>
+                    <tr>
+                        
+                        <th>NOME</th>
+                        <th>MÉTODO PAGAMENTO</th>
+                        <th>VALOR</th>
+                        <th>DATA</th>
+                        <th>STATUS</th>
+                        <th>ANALISAR</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+
+                
+                    <tr>
+                        <td>Clovis</td>
+                        <td>PIX</td>
+                        <td>R$ 10,00</td>
+                        <td>02/11/2021</td>
+                        <td>Em fila</td>
+                        <td>
+                            <img src='/aprovado.png'  />
+
+
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                            <img src='/reprovado.png'/>
+
+                            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+                            <img src='/analise.png'/>
+                        </td>
+                    </tr>
+                
+                </tbody>
+            </table>
+        </div>
      </div>
        </main>
     )
