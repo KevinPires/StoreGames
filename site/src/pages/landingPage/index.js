fbe1df620572e87b3122ec712272b555a63f8a33
vidal007 <guilhermevidalpekka@gmail.com>
2022-11-04 09:44:12 -0300

    carrocel pronto 100%

--- site/src/pages/landingPage/index.js
+++ site/src/pages/landingPage/index.js
@@ -20,36 +20,40 @@ export default function LandingPage() {
     const [jogos, setJogos] = useState([]);
     const [jogofinal, setJogoFinal] = useState(4)
     const currentJogos = jogos.slice(0, jogofinal);
-    
+
 
     const [exibirfaixa, setExibirFaixa] = useState('faixa-dois')
 
-    let count = 1;
-    document.getElementById("radio1").checked = true;
 
-    setInterval(function(){
-        nextImage();
-    }, 2000)
-
-    function nextImage(){
-        count++;
-        if(count > 4){
-            count = 1;
-       }
-
-       document.getElementById("radio" + count).checked = true;
+    let count = 0;
+    function nextImage() {
+        if (count > 4) {
+            count = 0;
+        }
+        count++
+        document.getElementById("radio" + count).checked = true;
     }
+
+    useEffect(() => {
+        const interval = setInterval(function () {
+            nextImage();
+        }, 2500);
+
+        return () => clearInterval(interval);
+    }, [])
+
+    
 
     // Provavelmenmte a pagina n vai carregar por causa dessa função em cima, qualquer coisa so comentar
 
     function exibirMais() {
-        setExibirFaixa('faixa-dois-expandida') 
+        setExibirFaixa('faixa-dois-expandida')
         setJogoFinal(10)
     }
 
-  useEffect(() => {
-    AOS.init({duration : 2000})
-  }, [])
+    useEffect(() => {
+        AOS.init({ duration: 2000 })
+    }, [])
 
     async function mostrarJogo() {
         const x = await listarJogosDestaque()
@@ -59,10 +63,10 @@ export default function LandingPage() {
     useEffect(() => {
         mostrarJogo()
     }, [])
-    
+
     return (
         <main className='landing-page'>
-            <HeaderLoja/>
+            <HeaderLoja />
 
             <section data-aos="fade-up" className='faixa-um'>
 
@@ -81,33 +85,33 @@ export default function LandingPage() {
                 </div>
 
                 <div data-aos="slide-right" className="listra-maiorr">
-                    <img id='listra-maior'src="./listramaior.svg" alt="" />
+                    <img id='listra-maior' src="./listramaior.svg" alt="" />
                 </div>
 
             </section>
 
-            <section  className={exibirfaixa}>
+            <section className={exibirfaixa}>
 
                 <div className="f2-texto-format">
                     <h1 data-aos="slide-up">Jogos mais vendidos</h1>
-                    <p  data-aos="slide-up">Veja os jogos mais vendidos!</p>
+                    <p data-aos="slide-up">Veja os jogos mais vendidos!</p>
                 </div>
 
                 <div className="container-jogos">
-                    <CardJogo jogo={jogos} jogos={currentJogos}/>
+                    <CardJogo jogo={jogos} jogos={currentJogos} />
                 </div>
-        
+
                 <button data-aos="zoom-in" onClick={exibirMais}><b>Mostrar mais</b></button>
 
                 <hr data-aos="fade" />
 
-            </section>  
+            </section>
 
             <section data-aos="fade-up" className="faixa-tres">
                 <div className="container-hermes">
                     <img data-aos="slide-up" src="/hermes.png" alt="" />
                 </div>
-                
+
                 <div data-aos="fade" className="box-texto-f3">
                     <div className="f3-texto-format ">
                         <h1 data-aos="slide-right">Rapidez e eficiência</h1>
@@ -120,60 +124,60 @@ export default function LandingPage() {
 
             <section className='jogos-populares'>
                 <div className='titulo-jogos'>
-                <div><h1 data-aos="slide-right">Jogos mais jogados</h1></div>
-                <div><p data-aos="slide-up">Explore nossa biblioteca com os jogos mais populares! </p></div>
+                    <div><h1 data-aos="slide-right">Jogos mais jogados</h1></div>
+                    <div><p data-aos="slide-up">Explore nossa biblioteca com os jogos mais populares! </p></div>
                 </div>
-                
-                 <div className='slider'>
+
+                <div className='slider'>
 
                     <div className='slides'>
 
-                        <input type='radio' name='radio-btn' id='radio1'/>
-                        <input type='radio' name='radio-btn' id='radio2'/>
-                        <input type='radio' name='radio-btn' id='radio3'/>
-                        <input type='radio' name='radio-btn' id='radio4'/>
+                        <input type='radio' name='radio-btn' id='radio1' />
+                        <input type='radio' name='radio-btn' id='radio2' />
+                        <input type='radio' name='radio-btn' id='radio3' />
+                        <input type='radio' name='radio-btn' id='radio4' />
 
                         <div className='slide-frist' onClick={() => Navigate('/produto/275/detalhe')}>
-                            <img src='/roct.jpg' alt='img1'/>
+                            <img src='/roct.jpg' alt='img1' />
                         </div>
 
                         <div className='slide' onClick={() => Navigate('/produto/272/detalhe')}>
-                            <img src='/min.jpg' alt='img2'/>
+                            <img src='/min.jpg' alt='img2' />
                         </div>
 
                         <div className='slide' onClick={() => Navigate('/produto/240/detalhe')}>
-                            <img src='/ride.jpg' alt='img3'/>
+                            <img src='/ride.jpg' alt='img3' />
                         </div>
 
                         <div className='slide' onClick={() => Navigate('/produto/234/detalhe')}>
-                            <img src='/fafi.jpg' alt='img4'/>
+                            <img src='/fafi.jpg' alt='img4' />
                         </div>
 
-                      <div className='navigation-auto'>
-                        <div className='auto-btn1'></div>
-                        <div className='auto-btn2'></div>
-                        <div className='auto-btn3'></div>
-                        <div className='auto-btn4'></div>
-                      </div>
+                        <div className='navigation-auto'>
+                            <div className='auto-btn1'></div>
+                            <div className='auto-btn2'></div>
+                            <div className='auto-btn3'></div>
+                            <div className='auto-btn4'></div>
+                        </div>
                     </div>
 
                     <div className='manual-navigation'>
-                    <label for='radio1' className='manual-btn'></label>
-                    <label for='radio2' className='manual-btn'></label>
-                    <label for='radio3' className='manual-btn'></label>
-                    <label for='radio4' className='manual-btn'></label>
-                 </div>
-                 </div>
+                        <label for='radio1' className='manual-btn'></label>
+                        <label for='radio2' className='manual-btn'></label>
+                        <label for='radio3' className='manual-btn'></label>
+                        <label for='radio4' className='manual-btn'></label>
+                    </div>
+                </div>
 
-                
-                
 
-               
+
+
+
             </section>
 
             <section data-aos="fade" className="faixa-cadastro">
                 <div data-aos="zoom-in" className="container-lobo">
-                <Link to='/usuario/cadastro'><button data-aos="fade" ><b>Cadastre-se agora!</b></button></Link>
+                    <Link to='/usuario/cadastro'><button data-aos="fade" ><b>Cadastre-se agora!</b></button></Link>
                 </div>
 
                 <div data-aos="slide-up" className="container-boxs">
@@ -192,14 +196,14 @@ export default function LandingPage() {
                         </div>
                     </div>
                 </div>
-            </section> 
+            </section>
 
             <section data-aos="fade-up" className="descontos flexboxcolumn">
                 <h1 data-aos="slide-down">Não perca os melhores descontos!</h1>
                 <Link to='/usuario/cadastro'><button><b>Cadastre-se agora!</b></button></Link>
             </section>
 
-        <Rodape/>
+            <Rodape />
         </main>
     )
 }
